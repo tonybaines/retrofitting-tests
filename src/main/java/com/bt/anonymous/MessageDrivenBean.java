9f2816aa5e23cbcd8ed20185200de408c069505a
tony.baines@bt.com <>
2009-09-07 12:04:49 +0100

    Savepoint

--- src/main/java/com/bt/anonymous/MessageDrivenBean.java
+++ src/main/java/com/bt/anonymous/MessageDrivenBean.java
@@ -1,13 +1,29 @@
 package com.bt.anonymous;
 
+/*
+ * In real life this is an enterprise EJB with an interesting extra method 
+ */
 public class MessageDrivenBean {
 
 	public static final String JMS_FACTORY = null;
 	public static final String REDIRECT_QUEUE = null;
 	public static final String RESPONSE_QUEUE = null;
-	public static void sendMail(String message) {
-		// TODO Auto-generated method stub
-		
-	}
+	public static boolean sendMail(String message) {
+		try
+		{                                                                
+			String[] cmd = {"/bin/sh",
+					"-c", 
+					"echo $HOSTNAME' : '`date`' : Internal problem  during MDB execution.\n'"
+						+message
+						+"'\n Please check the logs for *** for more details ' | mailx -s Error_in_****** [hardcoded email removed] "};
 
+			Runtime.getRuntime().exec(cmd);  
+			return true;
+		}
+		catch (Exception err) 
+		{  
+			return false;                                                                                                                                                                       
+		}
+	}
 }
+
--- src/test/java/com/bt/anonymous/TestXMLSender.java
+++ src/test/java/com/bt/anonymous/TestXMLSender.java
@@ -2,9 +2,28 @@ package com.bt.anonymous;
 
 import static org.junit.Assert.*;
 
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.TextMessage;
+// Need to be careful with the imports to avoid clashes
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.anyBoolean;
+import static org.easymock.EasyMock.anyInt;
+import static org.easymock.EasyMock.anyObject;
+import static org.powermock.api.easymock.PowerMock.*;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(JNDILookup.class)
 public class TestXMLSender {
 
 	@Test
@@ -20,15 +39,57 @@ public class TestXMLSender {
 	
 	// Go with the simplest happy path scenario to begin with
 	@Test
-	public void shouldSendtheSuppliedMessageToTheResponseQueueByDefault() {
+	public void shouldSendtheSuppliedMessage() throws JMSException {
 		// Simple string parameters - guess at values
 		String text = "Test message";
 		String messageID = "TEST-MESSAGE-ID";
 		String senderFlag = "";
 		// An easy class to get a suitable instance
 		Logger logger = org.apache.log4j.LogManager.getLogger("TEST-LOGGER");
-		new XMLSender(logger, text, messageID, senderFlag);
-		// 1. Run as is to check for hidden dependencies
+		// 1. Run as is to check for hidden dependencies (no exceptions)
+		//new XMLSender(logger, text, messageID, senderFlag);
+		// 2. Attempt to sense and assert behaviour - was a message sent?
+		//     The serviceProvider (JNDILookup) provides the various collaborators - but
+		//     access is through a static factory method getInstance()
+		//     Standard mock frameworks won't help => PowerMock
+		mockStatic(JNDILookup.class);
+		// "Unexpected method call getQueueConnectionFactory(null):" - do we care about these calls?
+		// JNDILookup mockJndiLookup = createMock(JNDILookup.class);
+		//  - not really, but we need to make sure we can sense the queueSender.send(textMessage);
+		JNDILookup mockJndiLookup = createNiceMock(JNDILookup.class);
+		QueueConnectionFactory mockQCF = createNiceMock(QueueConnectionFactory.class);
+		QueueConnection mockQC = createNiceMock(QueueConnection.class);
+		QueueSession mockQS = createNiceMock(QueueSession.class);
+		QueueSender mockQSndr = createNiceMock(QueueSender.class);
+		TextMessage mockTextMsg = createNiceMock(TextMessage.class);
 		
+		expect(mockJndiLookup.getQueueConnectionFactory(null)).andReturn(mockQCF);
+		expect(mockQCF.createQueueConnection()).andReturn(mockQC);
+		expect(mockQC.createQueueSession(anyBoolean(), anyInt())).andReturn(mockQS);
+		expect(mockQS.createTextMessage()).andReturn(mockTextMsg);
+		expect(mockQS.createSender((Queue) anyObject())).andReturn(mockQSndr);
+		
+		// Finally, something we care about - these are assertions about behaviour 
+		//  (so long as verify() is called)
+		// First try it out with a bad value
+		// mockTextMsg.setText("rubbish");
+		mockTextMsg.setText(text);
+		mockTextMsg.setStringProperty("messageId", messageID);
+		mockQSndr.send(mockTextMsg);
+		
+		replay(mockQCF, mockQC, mockQS, mockQSndr, mockTextMsg);
+		
+		// Intercept the factory method and inject our mock(s)
+		expect(JNDILookup.getInstance()).andReturn(mockJndiLookup);
+		
+		// Get the mocks ready to use
+		replay(JNDILookup.class, mockJndiLookup);
+
+		new XMLSender(logger, text, messageID, senderFlag);
+
+		// Check our expectations
+		verify(JNDILookup.class);
+		verify(mockJndiLookup);
+		verify(mockQCF, mockQC, mockQS, mockQSndr, mockTextMsg);
 	}
 }
